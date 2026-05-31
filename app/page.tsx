789e5c2b4d7b95769b6673224c053140d5b8f363
lori corpuz <loricorpuz@gmail.com>
2026-05-31 12:26:23 -0400

    Make aretetec.com root show Arete landing (Armstrong only, hide salons/mistral)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Header from '@/components/portfolio/Header'
-import AboutSection from '@/components/portfolio/AboutSection'
-import Footer from '@/components/portfolio/Footer'
+import AretePage from '@/app/arete/page'
 
 export default function Home() {
-  return (
-    <div className="max-w-[640px] mx-auto py-20 px-6 max-[480px]:py-12 max-[480px]:px-5">
-      <Header />
-      <AboutSection />
-      <Footer />
-    </div>
-  )
+  return <AretePage />
 }
