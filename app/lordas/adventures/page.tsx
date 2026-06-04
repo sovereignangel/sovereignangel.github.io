db42a02ac98b19f87fe3676a764beda4d9e6f01d
lori corpuz <loricorpuz@gmail.com>
2026-06-03 22:52:39 -0400

    Make adventures route dynamic

--- app/lordas/adventures/page.tsx
+++ app/lordas/adventures/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useEffect, useCallback } from 'react'
 import { AdventuresView } from '@/components/lordas/AdventuresView'
 import type { SummerPlan, AdventureComment, RelationalSpeaker } from '@/lib/types'
